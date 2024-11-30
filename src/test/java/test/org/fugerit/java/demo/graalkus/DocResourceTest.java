904294511d235cfb9bba926b8555b5d2d260fb12
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-12-01 00:02:26 +0100

    Step 02 Going AOT

--- src/test/java/test/org/fugerit/java/demo/graalkus/DocResourceTest.java
+++ src/test/java/test/org/fugerit/java/demo/graalkus/DocResourceTest.java
@@ -2,6 +2,7 @@ package test.org.fugerit.java.demo.graalkus;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
 
 import static io.restassured.RestAssured.given;
 
@@ -22,10 +23,13 @@ class DocResourceTest {
     void testAsciiDoc() {
         given().when().get("/doc/example.adoc").then().statusCode(200);
     }
+
     @Test
+    @DisabledInNativeImage
     void testPdf() {
         given().when().get("/doc/example.pdf").then().statusCode(200);
     }
+
     @Test
     void testCsv() {
         given().when().get("/doc/example.csv").then().statusCode(200);
