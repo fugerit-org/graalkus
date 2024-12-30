4fe10c154547ebd7f62515dddd4c70d3fdfce738
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-12-30 11:11:56 +0100

    register for reflection

--- src/main/java/org/fugerit/java/demo/graalkus/AppInit.java
+++ src/main/java/org/fugerit/java/demo/graalkus/AppInit.java
@@ -1,12 +1,16 @@
 package org.fugerit.java.demo.graalkus;
 
 import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import org.fugerit.java.doc.base.config.InitHandler;
 
+@Slf4j
 @ApplicationScoped
+@RegisterForReflection(targets = { DocHelper.class, People.class })
 public class AppInit {
 
     @Inject
